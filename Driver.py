904ca244f907f35eb69b5b683bd036146814d416
santiago_andaluz <santiago_andaluz_ruiz@baylor.edu>
2017-03-02 18:40:13 -0600

    Starting prog 2

---
+++ Driver.py
@@ -0,0 +1,5 @@
+from wordnet_newdist import *
+
+word_path_similarity("Lemon", "Oragnge")
+word_lch_similarity("Lemon", "Oragnge")
+word_wup_similarity("Lemon", "Oragnge")
---
+++ wordnet_newdist.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk.corpus import wordnet
+def word_path_similarity(s,t,simulate_root=True):
+	print("Testing word_path_similarity")
+	return
+
+def word_lch_similarity(s,t,simulate_root=True):
+	print("Testing word_lch_similarity")
+	return
+
+def word_wup_similarity(s,t,simulate_root=True):
+	print("Testing word_wup_similarity")
+	return
